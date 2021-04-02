9c0ff2f87258eefcb5cf9ad8039574640756a2e3
kasper <kasperfriangt@gmail.com>
2021-04-03 01:19:25 +0200

    added personal page

---
+++ app/src/main/java/com/example/workouttrackertoothbrook/ui/personal/personalFragment.java
@@ -0,0 +1,38 @@
+package com.example.workouttrackertoothbrook.ui.personal;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.workouttrackertoothbrook.R;
+
+public class personalFragment extends Fragment {
+
+    private PersonalViewModel mViewModel;
+
+    public static personalFragment newInstance() {
+        return new personalFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_personal, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PersonalViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
